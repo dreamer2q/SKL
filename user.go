cc8d63efe682f53f382223628a9697529076ec2d
Jack Li <dreamer22qq@gmail.com>
2019-12-20 23:58:56 +0800

    update

--- user.go
+++ user.go
@@ -1,6 +1,7 @@
 package skl
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -10,39 +11,36 @@ import (
 )
 
 type UserInfo struct {
-	ID string `json:"id"`
+	ID       string `json:"id"`
 	UserName string `json:"userName"`
-	UserType int `json:"userType"`
-	UnitID string `json:"unitId"`
+	UserType int    `json:"userType"`
+	UnitID   string `json:"unitId"`
 	UnitCode string `json:"unitCode"`
 	UnitName string `json:"unitName"`
-	Grade string `json:"grade"`
-	ClassNo string `json:"classNo"`
-	Sex string `json:"sex"`
-	Major string `json:"major"`
+	Grade    string `json:"grade"`
+	ClassNo  string `json:"classNo"`
+	Sex      string `json:"sex"`
+	Major    string `json:"major"`
 	//RoleList interface{} `json:"roleList"`
 }
 
 type User struct {
-	ID  string
-	User *UserInfo
+	ID    string
+	User  *UserInfo
 	Token string
 	Group string
 	app   *gorequest.SuperAgent
 }
 
-
 //get a *User with empty params, login is required to continue next operations.
-func NewUser(id,group string) *User {
-	user := &User{
+func NewUser(id, group string) *User {
+	return &User{
 		ID:    id,
 		User:  nil,
 		Token: "",
 		Group: group,
 		app:   gorequest.New(),
 	}
-	user.app.Set("User-Agent", "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.79 Mobile Safari/537.36")
-	return user
 }
 
 //login in with HDU, and get a token for SKL
@@ -72,10 +70,10 @@ func (s *User) Login(user, pass string) error {
 	//urlquery := "https://cas.hdu.edu.cn/cas/login?state=&service=https%3A%2F%2Fskl.hdu.edu.cn%2Fapi%2Fcas%2Flogin%3Findex%3D"
 	//urlquery := "http://116.62.36.144/"
 	s.app.Post(url)
-	s.app.Type("form") 	//s.app.Set("Content-Type", "application/x-www-form-urlencoded")
+	s.app.Type("form") //s.app.Set("Content-Type", "application/x-www-form-urlencoded")
 
 	s.app.Send(map[string]string{
-		"rsa":       encStr(user, pass, lt[1]),	//this encStr is very slow...
+		"rsa":       encStr(user, pass, lt[1]), //this encStr is very slow...
 		"ul":        fmt.Sprintf("%d", len(user)),
 		"pl":        fmt.Sprintf("%d", len(pass)),
 		"lt":        lt[1],
@@ -112,29 +110,107 @@ func (s *User) Login(user, pass string) error {
 //login in with Token
 func (s *User) LoginToken(token string) error {
 	s.Token = token
-	s.app.Set("X-Auth-Token",s.Token)
+	return s.loadInfo()
+}
+
+//isTokenValid
+func (s *User) TokenValid() error {
 	return s.loadInfo()
 }
 
 func (s *User) loadInfo() error {
-	//s.app.Set("X-Auth-Token",s.Token)
+
 	url := "https://skl.hdu.edu.cn/api/userinfo"
-	resp,body,errs := s.app.Get(url).End()
+	resp, body, err := s.get(url)
 	if resp == nil || resp.StatusCode != 200 {
-		return errors.New(fmt.Sprintf("%v",errs))
+		return errors.New(body)
 	}
 	userInfo := &UserInfo{}
-	err := json.Unmarshal([]byte(body),userInfo)
+	err = json.Unmarshal([]byte(body), userInfo)
 	if err != nil {
 		return err
 	}
+	s.User = userInfo
 	return nil
 }
 
-func (s *User) Test() {
-	s.loadInfo()
+func (s *User) get(url string) (gorequest.Response, string, error) {
+	req := s.app.Get(url)
+	req.Set("User-Agent", "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.79 Mobile Safari/537.36")
+	req.Set("X-Auth-Token", s.Token)
+	req.Set("Origin", "https://skl.hduhelp.com")
+	req.Set("Referer", "https://skl.hdu.edu.cn/")
+
+	resp, body, errs := req.End()
+	return resp, body, errors.New(fmt.Sprintf("%v", errs))
 }
 
-func (* User) CheckCode(code string) {
+func (s *User) CheckCode(code string, maxRetry int) error {
 
+	for i := 0; i < maxRetry; i++ {
+		url := fmt.Sprintf("https://skl.hdu.edu.cn/api/checkIn/code-check-in?code=%s", code)
+		resp, body, err := s.get(url)
+		if resp == nil || resp.StatusCode != 200 {
+			return err
+		}
+
+		b6Img := base64.StdEncoding.EncodeToString([]byte(body))
+		retCode, err := s.recCaptcha(string(b6Img))
+		if err != nil {
+			return err
+		}
+		err = s.validCode(retCode)
+		if err == nil {
+			return nil
+		}
+		if strings.Contains(err.Error(), "401") {
+			return errors.New("签到码无效")
+		}
+		if !strings.Contains(err.Error(), "400") {
+			//未知错误
+			return err
+		}
+		//验证码识别错误，重试
+	}
+	return errors.New("reach the max retry times")
+}
+
+func (s *User) recCaptcha(img string) (string, error) {
+	url := "http://localhost:5001/captcha"
+	req := gorequest.New().Post(url)
+	req.Type("form")
+	data := map[string]string{
+		"data": img,
+	}
+	req.Send(data)
+	resp, body, errs := req.End()
+	if resp == nil || resp.StatusCode != 200 {
+		return "", errors.New(fmt.Sprintf("%v", errs))
+	}
+	var ret = &struct {
+		Err  int    `json:"err"`
+		Msg  string `json:"msg"`
+		Data string `json:"data"`
+	}{}
+	err := json.Unmarshal([]byte(body), ret)
+	if err != nil {
+		return "", err
+	}
+	if ret.Err != 0 {
+		return "", errors.New(ret.Msg)
+	}
+	return ret.Data, nil
+}
+
+func (s *User) validCode(code string) error {
+	url := fmt.Sprintf("https://skl.hdu.edu.cn/api/checkIn/valid-code?code=%s", code)
+	resp, _, err := s.get(url)
+	if resp == nil {
+		return err
+	}
+
+	if resp.StatusCode != 200 { //400 验证码不正确，401，签到码无效，200 ？
+		return errors.New(fmt.Sprintf("%d", resp.StatusCode))
+	}
+	return nil
 }
